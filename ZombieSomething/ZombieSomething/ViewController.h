6938210962bbab904e64ee171551e6579ea7be6a
Allison Allain <allison.allain@gmail.com>
2014-05-12 19:06:46 -0600

    initial checkin'

---
+++ ZombieSomething/ZombieSomething/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZombieSomething
+//
+//  Created by Allison Allain on 5/12/14.
+//  Copyright (c) 2014 Allison Wonderland Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ZombieSomething/ZombieSomethingTests/ZombieSomethingTests.m
@@ -0,0 +1,34 @@
+//
+//  ZombieSomethingTests.m
+//  ZombieSomethingTests
+//
+//  Created by Allison Allain on 5/12/14.
+//  Copyright (c) 2014 Allison Wonderland Games. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZombieSomethingTests : XCTestCase
+
+@end
+
+@implementation ZombieSomethingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
