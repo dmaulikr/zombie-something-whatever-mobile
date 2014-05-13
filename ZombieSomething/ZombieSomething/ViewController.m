5621fde2662b68249b7a55b578a26fe7be499032
Allison Allain <allison.allain@gmail.com>
2014-05-12 20:29:50 -0600

    adding ibeacons

--- ZombieSomething/ZombieSomething/ViewController.m
+++ ZombieSomething/ZombieSomething/ViewController.m
@@ -10,7 +10,14 @@
 #import <Firebase/Firebase.h>
 
 @interface ViewController ()
+@property (strong, nonatomic) CLBeaconRegion *beaconRegion;
+@property (strong, nonatomic) NSDictionary *beaconPeripheralData;
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager;
 
+@property (weak, nonatomic) IBOutlet UILabel *uuidLabel;
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *identityLabel;
 @end
 
 @implementation ViewController
@@ -19,12 +26,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setLabels];
+    
     
     // Create a reference to a Firebase locationzombiefire.firebaseio.com
     Firebase* f = [[Firebase alloc] initWithUrl:@"https://zombiefire.firebaseio.com/"];
-    
-    // Write data to Firebase
-    [f setValue:@"Do you have data? You'll love Firebase."];
     
     // Read data and react to changes
     [f observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
@@ -34,10 +40,46 @@
     
 }
 
+- (void)setLabels {
+    self.uuidLabel.text = self.beaconRegion.proximityUUID.UUIDString;
+    self.majorLabel.text = [NSString stringWithFormat:@"%@", self.beaconRegion.major];
+    self.minorLabel.text = [NSString stringWithFormat:@"%@", self.beaconRegion.minor];
+    self.identityLabel.text = self.beaconRegion.identifier;
+}
+
+
+- (void)initBeacon {
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"23542266-18D1-4FE4-B4A1-23F8195B9D39"];
+    self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                major:1
+                                                                minor:1
+                                                           identifier:@"com.devfright.myRegion"];
+}
+
+- (IBAction)transmitBeacon:(UIButton *)sender {
+    self.beaconPeripheralData = [self.beaconRegion peripheralDataWithMeasuredPower:nil];
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Delegate
+
+-(void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn) {
+        NSLog(@"Powered On");
+        [self.peripheralManager startAdvertising:self.beaconPeripheralData];
+    } else if (peripheral.state == CBPeripheralManagerStatePoweredOff) {
+        NSLog(@"Powered Off");
+        [self.peripheralManager stopAdvertising];
+    }
+}
+
 
 @end
